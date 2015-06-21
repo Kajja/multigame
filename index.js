e9a2059a0e342e707858a381beb5b3aad0e27afb
Mikael Feuk <mikael.feuk@yahoo.se>
2015-06-21 17:15:52 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+exports.Manager = require('./lib/Manager');
+exports.Game = require('./lib/Game');
+exports.Observer = require('./lib/Observer');
+exports.Player = require('./lib/Player');
+exports.Server = require('./lib/Server');
+exports.verifyer = require('./lib/verifyer');
